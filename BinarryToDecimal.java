9da631b9bf7cab5b370663cf2bd1d736679fc3c5
Barkhad Awil <awilbarkhad5@gmail.com>
2023-11-27 08:27:14 -0800

    add comments into the asnwer

--- BinarryToDecimal.java
+++ BinarryToDecimal.java
@@ -16,13 +16,14 @@ public class BinarryToDecimal{
     public static int displayDecimal(String str){
         int[] arr = new int[str.length()];
 
-        for(int i =0; i< str.length(); i++){
+        //populate the "arr" array
+        for(int i = 0; i < str.length(); i++){
             arr[i] = Character.getNumericValue(str.charAt(i));
         }
 
+        //reverse the "arr" array
         int start = 0;
         int end = arr.length -1;
-
         while(start < end){
             int temp = arr[start];
             arr[start] = arr[end];
@@ -30,9 +31,9 @@ public class BinarryToDecimal{
 
             start++;
             end --;
-
         }
 
+        //chane the binarry to decimal
         int sum = 0;
         for(int i = 0; i < arr.length; i++){
             int base = 2;
@@ -43,8 +44,9 @@ public class BinarryToDecimal{
     }
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter binnary Number (eg. 10110): ");
+        System.out.print("Enter a binnary Number: ");
         String binnaryNum = scanner.nextLine();
+
         int value = displayDecimal(binnaryNum);
         System.out.println(value);
 
