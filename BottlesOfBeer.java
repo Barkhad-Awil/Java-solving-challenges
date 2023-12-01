3cacfa80e0513d0787d743175c3e556f82dbf175
Barkhad Awil <awilbarkhad5@gmail.com>
2023-12-01 10:53:19 -0800

    updated excercise 2.2

--- BottlesOfBeer.java
+++ BottlesOfBeer.java
@@ -1,53 +1,38 @@
 /*
 Consider the first verse of the song "99 Bottles of Beer":
-99 bottles of beer on the wall,
-99 bottles of beer,
-ya' take one down, ya' pass it around,
-98 bottles of beer on the wall.
-Subsequent verses are identical except that the number of bottles gets smaller
-by one in each verse, until the last verse:
-No bottles of beer on the wall,
-no bottles of beer,
-ya' can't take one down, ya' can't pass it around,
-'cause there are no more bottles of beer on the wall!
-And then the song (finally) ends.
+99 bottles of juice on the wall,
+99 bottles of juice,
+Take one down, and pass it around,
+98 bottles of juice on the wall.
+
 Exercise 2.2: Write a program that displays the entire lyrics of "99 Bottles of Beer". Your
 program should include a  method that does the hard part, but you
 might want to write additional methods to separate other parts of the program.
-As you develop your code, test it with a small number of verses, like 3.
+
 */
 
 
-public class BottlesOfBeer {
-
-    public static void verse(String verse1, String verse2) {
-        // Extract starting number
-        int num11 = Integer.parseInt(verse1.substring(0, 2));
-
-        int firstNum = verse1.indexOf("99");
-        int lastNum = verse1.lastIndexOf("99");
-        String first = verse1.substring(firstNum + 2, lastNum);
-        String last = verse1.substring(lastNum + 2);
-
-        // Extract parts of verse2
-        int verse2num = verse2.indexOf("99");
-        int getVerse2Num = Integer.parseInt(verse2.substring(verse2num, verse2num + 2));
-        String firstPV2 = verse2.substring(0, verse2num);
-        String lastPV2 = verse2.substring(verse2num + 2);
-
-        System.out.println("Bottles of Beer Song starting...");
-        for (int i = num11; i > 0; i--) {
-            System.out.println(i + first + i + last);
-            if (i > 1) {
-                System.out.println(firstPV2 + (i - 1) + lastPV2);
-            }
+class Bottle{
+    public static void  singSong(int numberOfBottles){
+        if(numberOfBottles > 0){
+            displayVerse(numberOfBottles);
             System.out.println();
+            singSong(numberOfBottles -1);
+        } else {
+            System.out.println("No more bottles of juice on the wall, no more bottles of juice.");
+            System.out.println("Go to the store and buy some more, 11 bottles of juice on the wall.");
         }
     }
 
-    public static void main(String[] args) {
-        String verse1 = "99 bottles of juice on the wall, 99 bottles of juice.";
-        String verse2 = "Take one down and pass it around, 99 bottles of juice on the wall";
-        verse(verse1, verse2);
+    public static void displayVerse(int numberOfBottles){
+        String plural  = (numberOfBottles == 1)? "" : "s";
+        System.out.println(numberOfBottles + " bottle" + plural + " juice on the wall, " + 
+        numberOfBottles + " bottle" + plural + " of Juice");
+        System.out.println("Take one down and pass it around, " + (numberOfBottles - 1 == 0 ? "no more" : numberOfBottles - 1) + " bottle" + plural + " of juice on the wall.");
+    }
+    public static void main(String[] args){
+        int numberOfBottles = 99;
+        singSong(numberOfBottles);
     }
 }
+
