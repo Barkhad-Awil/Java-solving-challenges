d0eb49746cb1171a3149cd684cd682a7369cd32c
Barkhad Awil <awilbarkhad5@gmail.com>
2023-12-22 20:24:17 -0800

    updated Q3.6

--- ValOfVar.java
+++ ValOfVar.java
@@ -1,15 +1,23 @@
-public class ValOfVar{
+/*
+ * Question 3.6: Calculate the value of variable x after operations,
+ * using the provided array of Strings.
+ * Input: operations = ["--X", "X++", "X++"]
+ * Output: 1
+ */
 
-    public static int findValueOfValiable(String[] operations){
+public class ValOfVar {
+
+    public static int findValueOfVariable(String[] operations) {
         int x = 0;
-        for(int i = 0; i < operations.length; i++){
-            x += + (44 - operations[i].charAt(1));
+        for (int i = 0; i < operations.length; i++) {
+            x += +(44 - operations[i].charAt(1));
         }
         return x;
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         String[] operations = {"x--", "++x", "x++"};
-        int value = findValueOfValiable(operations);
+        int value = findValueOfVariable(operations);
         System.out.println(value);
     }
 }
