2e3e776b5656c11119c06920b747bbbaee8942b9
Barkhad Awil <awilbarkhad5@gmail.com>
2023-12-25 06:01:50 -0800

    added problem question 3.8

--- FindWordConChar.java
+++ FindWordConChar.java
@@ -1,6 +1,17 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+ * Question 3.8
+ * Problem Statement: Find Words Containing Character
+ * You are given a Java class FindWordConChar with a method findWordsContainCharacter. 
+ * The method takes two parameters:
+ * List<String> words: A list of words.
+ * char x: A character.
+ * The method is designed to find the indices of words in the list that contain the specified character x. 
+ * The indices are then stored in a list and returned as the result.
+ */
+
 public class FindWordConChar{
     public static List<Integer> findWordsContainCharacter(List<String> words, char x){
         List<Integer> ans = new ArrayList<>();
