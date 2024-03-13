fe30d8274b764ed49a5489b64319d7d4474cfef1
Barkhad Awil <awilbarkhad5@gmail.com>
2024-03-13 13:42:02 -0700

    refactored the class name

--- LeftToRightDiff.java
+++ LeftToRightDiff.java
@@ -1,4 +1,3 @@
-
 /* 
     Exercise 1.3 Write a method named areFactors that takes an integer n
     and an array of integers, and returns true if the numbers in the array are all
@@ -7,25 +6,25 @@
 
 import java.util.Arrays;
 
-public class Factors {
-    public static boolean areFactors(int n, int[] arr){
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i] % n != 0){
-                return false;
-            }
-        }
-        return true;      
+public class LeftToRightDiff {
+
+  public static boolean areFactors(int n, int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] % n != 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public static void main(String[] args) {
+    int[] arr = new int[10];
+    for (int i = 0; i < arr.length; i++) {
+      arr[i] = (i + 1) * 2;
     }
 
-    public static void main(String[] args){
-        int[] arr = new int[10];
-        for(int i = 0; i < arr.length ; i++){
-            arr[i] = (i + 1) * 2; 
-        }
-
-        System.out.println(Arrays.toString(arr));
-        boolean result = areFactors(2, arr);
-        System.out.println(result);
-    }
-    
+    System.out.println(Arrays.toString(arr));
+    boolean result = areFactors(2, arr);
+    System.out.println(result);
+  }
 }
